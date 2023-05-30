fee9ef5c56480300d7e8ff1dd84e697d95d22710
L-yo <eliott.duverger@hotmail.fr>
2023-05-30 14:44:36 +0200

    small breakthrought on data augmentation

--- source/utilitary.py
+++ source/utilitary.py
@@ -1,33 +1,79 @@
-import torch
-import torchvision
+import imgaug as ia
+import imgaug.augmenters as iaa
+from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
+import os
+import csv
+from PIL import Image
+import imageio.v2 as imageio 
 import matplotlib.pyplot as plt
+import numpy as np
 
-def dataload(batch_size_train, batch_size_test):
-    train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./', train=True, download=True,
-                                transform=torchvision.transforms.Compose([
-                                torchvision.transforms.ToTensor(),
-                                torchvision.transforms.Normalize(
-                                    (0.1307,), (0.3081,))
-                                ])),
-    batch_size=batch_size_train, shuffle=True)
 
-    test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./', train=False, download=True,
-                                transform=torchvision.transforms.Compose([
-                                torchvision.transforms.ToTensor(),
-                                torchvision.transforms.Normalize(
-                                    (0.1307,), (0.3081,))
-                                ])),
-    batch_size=batch_size_test, shuffle=True)
+def extract_bb(img_name, csv_name):
+    with open(csv_name, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        bb_list = []
+        for row in spamreader:
+            if row[5] == img_name:
+                bb_list.append(BoundingBox(int(row[1]), 
+                                            int(row[2]),
+                                            int(row[1]) + int(row[3]), 
+                                            int(row[2]) + int(row[4])))
+        return bb_list
 
-    return train_loader, test_loader
 
-def print_exemple(test_loader, i):
-    examples = enumerate(test_loader)
-    batch_idx, (example_data, example_targets) = next(examples)
-    fig = plt.figure()
 
-    plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
-    plt.title("Ground Truth: {}".format(example_targets[i]))
-    plt.show()
+
+ia.seed(1)
+
+rootdir = "../Data/"
+image_name = "IMG_20230529_134632.jpg"
+
+image = imageio.imread(rootdir + "photo-tarot/" + image_name)
+
+bbs = BoundingBoxesOnImage(extract_bb(image_name, rootdir + 'labels_tarot-bb_2023-05-29-09-58-51.csv'),
+                                        shape=image.shape)
+
+seq = iaa.Sequential([
+    iaa.Multiply((1.2, 1.5)), # change brightness, doesn't affect BBs
+    iaa.Affine(
+        translate_px={"x": 40, "y": 60},
+        rotate=(-45,45),
+        mode = "constant"
+        # scale=(0.2, 0.4)
+    ) # translate by 40/60px on x/y axis, and scale to 50-70%, affects BBs
+])
+
+
+# Augment BBs and images.
+image_aug, bbs_aug = seq(image=image, bounding_boxes=bbs)
+
+# print coordinates before/after augmentation (see below)
+# use .x1_int, .y_int, ... to get integer coordinates
+for i in range(len(bbs.bounding_boxes)):
+    before = bbs.bounding_boxes[i]
+    after = bbs_aug.bounding_boxes[i]
+
+# image with BBs before/after augmentation (shown below)
+image_before = bbs.draw_on_image(image, size=2)
+image_after = bbs_aug.draw_on_image(image_aug, size=4, color=[0, 0, 255])
+
+h, w = image_after.shape[:2]
+
+sem_map_after = np.dstack((image_after, np.zeros((h,w),dtype=np.uint8)+255))
+mBlack = (sem_map_after[:, :, 0:3] == [0,0,0]).all(2)
+sem_map_after[mBlack] = (0,255,0,0)
+
+
+foreground = Image.fromarray(np.uint8(sem_map_after)).convert('RGBA')
+background = Image.open(rootdir + "textures/braided/braided_0006.jpg")
+#resize, first image
+foreground = foreground.resize((180, 240))
+foreground_size = foreground.size
+background_size = background.size
+
+background.paste(foreground, (100,100), foreground)
+
+plt.imshow(np.asarray(background))
+plt.show()
+
