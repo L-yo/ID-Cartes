b919a64cda115021d897b804ab4a2ed6b53f400f
L-yo <eliott.duverger@hotmail.fr>
2023-07-08 22:04:03 +0200

    Solved transparent cards and stacking problems

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ import source.utilitary as uti
 # import source.custom_dataset as cust_data
 
 nb_carte = 3
+
 uti.augment_image("Data/", nb_carte, "labels_tarot-bb_2023-05-29-09-58-51.csv")
-
-
-ELIMINATE (0, 0, 0) PIXELS THAT GET EATEN BY THE MASK
--- source/utilitary.py
+++ source/utilitary.py
@@ -12,30 +12,30 @@ import pandas as pd
 
 def extract_bb(img_name, df):
     bb_list = []
-    rows =  df[df["image_name"] == img_name] 
+    rows =  df[df["image_name"] == img_name]
+
     for index, row in rows.iterrows():
         bb_list.append(BoundingBox(int(row["bbox_x"]), 
                                     int(row["bbox_y"]),  
                                     int(row["bbox_x"]) + int(row["bbox_width"]), 
-                                    int(row["bbox_y"]) + int(row["bbox_height"])))
+                                    int(row["bbox_y"]) + int(row["bbox_height"]),
+                                    label=rows.iloc[0]["label_name"]))
 
-    label = rows.iloc[0]["label_name"]
-    return bb_list, label
+    bb_list.append(BoundingBox(0, 0, int(row["image_width"]), int(row["image_height"]), label="carte"))
+    bb_list.reverse()
+    return bb_list
 
 def shift_bbs(bbs, x, y):
     bb_list = []
     for bb in bbs:
-        bb_list.append(BoundingBox(bb.x1_int+x, bb.y1_int+y, bb.x2_int+x, bb.y2_int+y))
+        bb_list.append(BoundingBox(bb.x1_int+x, bb.y1_int+y, bb.x2_int+x, bb.y2_int+y, bb.label))
     return bb_list
 
 def random_texture(rootdir):
     listdir = os.listdir(path=rootdir+'/textures')
     randcat = listdir[randrange(len(listdir))]
     texlist = os.listdir(path=rootdir+'/textures/'+randcat)
-    print(randcat)
-    print(len(texlist))
     randtext = rootdir+'/textures/'+randcat+'/'+texlist[randrange(len(texlist))]
-    
     return Image.open(randtext)
 
 def foreground_card(rootdir, row_nb, df):
@@ -44,20 +44,22 @@ def foreground_card(rootdir, row_nb, df):
     image_name = df.iloc[row_nb]["image_name"]
     image = imageio.imread(rootdir + "photo-tarot/" + image_name)
 
+    mBlack = (image[:, :, 0:3] == [0,0,0]).all(2)
+    image[mBlack] = (1,1,1)
 
             
-    bb_list, label = extract_bb(image_name, df)
+    bb_list = extract_bb(image_name, df)
     bbs = BoundingBoxesOnImage(bb_list, shape=image.shape)
 
     # Define transformations windows
     seq = iaa.Sequential([
-        iaa.Multiply((0.5, 1.5)), # change brightness, doesn't affect BBs
-        iaa.Resize((0.25,0.4)),
+        iaa.Multiply((1, 1.5)), # change brightness, doesn't affect BBs
+        iaa.Resize((0.25,0.3)),
         iaa.Affine(
             rotate=(-180, 180),
-            shear=(-40, 40),
+            shear=(-30, 30),
             mode = "constant",
-            scale=(0.4, 0.5))])
+            scale=(0.45, 0.52))])
 
     # Augment BBs and images.
     image_aug, bbs_aug = seq(image=image, bounding_boxes=bbs)
@@ -73,11 +75,25 @@ def foreground_card(rootdir, row_nb, df):
     
     return Image.fromarray(np.uint8(rgba)).convert('RGBA'), bbs_aug
 
+def detect_overlap(labels, carte, newlabs):
+
+    for label in labels:
+        check = carte.intersection(label, default="no_overlap")
+        if check != "no_overlap" and check.area > 50:
+            print("Deleting : "+label.label)
+            print(check.area)
+            labels.remove(label)
+
+    for lab in newlabs:
+        labels.append(lab)
+
+    return labels
+
 
 def augment_image(rootdir, nb_carte, csv_name):
 
-    label = {}
-    label["bbs"] = []
+    labels = []
+    bb_carte = None
     df = pd.read_csv(rootdir + csv_name)
     nb_row = df.shape[0]
 
@@ -86,12 +102,6 @@ def augment_image(rootdir, nb_carte, csv_name):
     for i in range(nb_carte):
         foreground, bbs_aug = foreground_card(rootdir, randrange(nb_row), df)
         w, h = foreground.size
-
-        # pixels = foreground.load() # create the pixel map
-        # for i in range(w): # for every pixel:
-        #     for j in range(h):
-        #         if pixels[i,j] == (255, 0, 0):
-        #             pixels[i,j] = (254, 0 ,0)
 
         shift_x = randrange(background.size[0] - w)
         if background.size[1] - h > 0:
@@ -102,15 +112,26 @@ def augment_image(rootdir, nb_carte, csv_name):
         background.paste(foreground, (shift_x, shift_y), foreground)   
         background = np.asarray(background)
 
-        bbs2 = BoundingBoxesOnImage(shift_bbs(bbs_aug, shift_x, shift_y), shape=background.shape)
-        background = bbs2.draw_on_image(background, size=2, color=[0, 0, 255])
-        background = Image.fromarray(np.uint8(background))
-        
-        label['bbs'].append(bbs2)
+        # labels.append(BoundingBoxesOnImage(shift_bbs(bbs_aug, shift_x, shift_y), shape=background.shape))
+        bbs = BoundingBoxesOnImage(shift_bbs(bbs_aug, shift_x, shift_y), shape=background.shape)
+        bb_carte = bbs[0]
+        new_labels = []
+        for label in bbs[1:]:
+            new_labels.append(label)
 
-    label['img'] = background
+        labels = detect_overlap(labels, bb_carte, new_labels)
+
+        
+        background = bb_carte.draw_on_image(background, size=1, color=[255, 0, 0])
+        background = Image.fromarray(np.uint8(background))
 
     background = np.asarray(background)
+    print(labels)
+    for bb in labels:
+        if bb.label == "carte":
+            background = bb.draw_on_image(background, size=1, color=[255, 0, 0])
+        else:
+            background = bb.draw_on_image(background, size=1, color=[0, 0, 255])
     plt.imshow(background)
-    plt.title(len(label['bbs']))
+    plt.title(len(labels))
     plt.show()
