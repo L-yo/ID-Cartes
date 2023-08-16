4c3276e9b5f4503e0a55bb243e9df79ae8157b1c
L-yo <eliott.duverger@hotmail.fr>
2023-08-16 14:08:51 +0200

    image generation complete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import source.utilitary as uti
+from random import randrange
 # import source.number_detector as ndec
 # import source.custom_dataset as cust_data
 
-nb_carte = 3
-
-uti.augment_image("Data/", nb_carte, "labels_tarot-bb_2023-05-29-09-58-51.csv")
+uti.augment_image("Data/", randrange(1,3), "labels_tarot-bb_2023-05-29-09-58-51.csv")
